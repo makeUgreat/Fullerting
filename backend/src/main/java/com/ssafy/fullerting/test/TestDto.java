65d277c9c864016e94dd59db9b8961199ee63c30
kjm <willom2c@naver.com>
2024-03-11 17:05:51 +0900

    [dev] : Swagger Config Setting & test

---
+++ backend/src/main/java/com/ssafy/fullerting/test/TestDto.java
@@ -0,0 +1,13 @@
+package com.ssafy.fullerting.test;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+public class TestDto {
+    private String code;
+    private String pwd;
+}
