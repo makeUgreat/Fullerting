181df81b1490c642f822ab2be845b9a270304be6
정준원 <junwon1131@naver.com>
2024-03-11 13:43:42 +0900

    main added

---
+++ backend/fullerting/src/test/java/com/ssafy/fullerting/FullertingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.fullerting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullertingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
