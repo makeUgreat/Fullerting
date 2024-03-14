6864acc977645b517afe27be4602f6b88968f055
오정민 <dhwjdals1324@gmail.com>
2024-03-14 09:31:26 +0900

    feat:메인컬러지정

--- frontend/src/styles/colors.ts
+++ frontend/src/styles/colors.ts
@@ -1,8 +1,9 @@
 export const COLORS = {
-  sub0: "#A0D8B3",
-  sub1: "#E5F9DB",
+  primary: "#A0D8B3",
+  sub0: "#E5F9DB",
   gray0: "#8C8C8C",
   gray1: "#C8C8C8",
   gray2: "#F4F4F4",
   red0: "#FF0000",
+  blue0: "#B6BDFF",
 };
