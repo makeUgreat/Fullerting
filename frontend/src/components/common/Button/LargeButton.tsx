d4ea7baead36c79901668a8216b5b3cdb5474e36
nayeon <kknyapple@gmail.com>
2024-03-14 21:55:20 +0900

    [feat] : BottomButton 제작
    하단에 띄우는 버튼

--- frontend/src/components/common/Button/LargeButton.tsx
+++ frontend/src/components/common/Button/LargeButton.tsx
@@ -1,4 +1,15 @@
+import styled from "styled-components";
 import Button from "./primaryButton";
+
+const ButtonBox = styled.div`
+  display: flex;
+  justify-content: center;
+  position: fixed;
+  bottom: 0;
+  padding: 1rem 0;
+  width: 22.5rem;
+  background-color: ${({ theme }) => theme.colors.white};
+`;
 
 interface LargeButtonType {
   onClick: () => void;
@@ -19,4 +30,20 @@ const LargeButton = ({ onClick, children }: LargeButtonType) => {
   );
 };
 
-export default LargeButton;
+const BottomButton = ({ onClick, children }: LargeButtonType) => {
+  return (
+    <ButtonBox>
+      <Button
+        width={19.875}
+        height={3.125}
+        color="#ffffff"
+        backgroundColor="#A0D8B3"
+        onClick={onClick}
+        fontWeight="bold"
+        children={children}
+      />
+    </ButtonBox>
+  );
+};
+
+export { LargeButton, BottomButton };
