144c59c24d47085979b7a3142f3f4c5c3a06b65f
nayeon <kknyapple@gmail.com>
2024-03-15 17:16:18 +0900

    [feat] : 임시 작물일지 목록

--- frontend/src/routes/router.tsx
+++ frontend/src/routes/router.tsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
 import LoginPage from "../pages/user/LoginPage";
 import JoinPage from "../pages/user/JoinPage";
+import CropPage from "../pages/diary/CropPage";
 
 const router = createBrowserRouter([
   {
@@ -33,6 +34,10 @@ const router = createBrowserRouter([
   {
     path: "/join",
     element: <JoinPage />,
+  },
+  {
+    path: "/diary",
+    element: <CropPage />,
   },
 ]);
 
