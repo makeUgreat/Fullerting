6aa32e9aa72c98211294be327fb8fb99389f7958
nayeon <kknyapple@gmail.com>
2024-03-15 17:42:15 +0900

    [feat] : MainPage 라우터 설정

--- frontend/src/routes/router.tsx
+++ frontend/src/routes/router.tsx
@@ -2,30 +2,12 @@ import { createBrowserRouter } from "react-router-dom";
 import LoginPage from "../pages/user/LoginPage";
 import JoinPage from "../pages/user/JoinPage";
 import CropPage from "../pages/diary/CropPage";
+import MainPage from "../pages/MainPage";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: (
-      <div style={{ margin: "0 auto", textAlign: "center" }}>
-        <div
-          style={{
-            fontSize: "1.875rem",
-            fontFamily: "seolleimcool",
-            margin: "5rem",
-          }}
-        >
-          풀러팅
-        </div>
-
-        <div style={{ fontSize: "0.875rem", fontWeight: "700" }}>
-          Bold 메인임!!!!
-        </div>
-        <div style={{ fontSize: "0.875rem", fontWeight: "400" }}>
-          Regular 메인임!!!!
-        </div>
-      </div>
-    ),
+    element: <MainPage />,
   },
   {
     path: "/login",
