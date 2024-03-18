1d4e74c258b52670e6e9dc8ced412bce71a9e254
nayeon <kknyapple@gmail.com>
2024-03-18 16:41:13 +0900

    [feat] : 작물 프로필, 다이어리 div로 구분

--- frontend/src/pages/diary/DiaryPage.tsx
+++ frontend/src/pages/diary/DiaryPage.tsx
@@ -8,6 +8,7 @@ import CropProfile from "../../components/diary/CropProfile";
 import Button from "../../components/common/Button/primaryButton";
 import RecognizeButton from "../../components/diary/RecognizeButton";
 import DiaryList from "../../components/diary/DiaryList";
+import MenuBar from "../../components/diary/MenuBar";
 
 interface CropType {
   packDiaryId: number;
@@ -19,6 +20,21 @@ interface CropType {
   packDiaryCreatedAt: string;
   cropTypeImgUrl: string;
 }
+
+const TopBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  gap: 0.5rem;
+`;
+
+const MiddleBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  gap: 0.5rem;
+  margin-top: 1.5rem;
+`;
 
 const ButtonBox = styled.div`
   display: flex;
@@ -42,28 +58,32 @@ const DiaryPage = () => {
 
   return (
     <>
-      <TopBar title="작물일지" showBack={false} showEdit={true} />
+      <TopBar title="작물일기" showBack={false} showEdit={true} />
       <LayoutMainBox>
         <LayoutInnerBox>
-          <CropProfile crop={crop} />
-          <ButtonBox>
-            <RecognizeButton />
-            <Button
-              onClick={handleClick}
-              width={9.5}
-              height={2.5625}
-              borderRadius={1.28125}
-              backgroundColor="#A0D8B3"
-              color="white"
-              fontSize="1"
-              fontWeight="bold"
-              text="종료하기"
-            />
-          </ButtonBox>
-          <div>menu</div>
-          <div>calendar</div>
-          <span>2024년</span>
-          <DiaryList />
+          <TopBox>
+            <CropProfile crop={crop} />
+            <ButtonBox>
+              <RecognizeButton />
+              <Button
+                onClick={handleClick}
+                width={9.5}
+                height={2.5625}
+                borderRadius={1.28125}
+                backgroundColor="#A0D8B3"
+                color="white"
+                fontSize="1"
+                fontWeight="bold"
+                text="종료하기"
+              />
+            </ButtonBox>
+          </TopBox>
+          <MiddleBox>
+            <MenuBar />
+            <div>calendar</div>
+            <span>2024년</span>
+            <DiaryList />
+          </MiddleBox>
         </LayoutInnerBox>
       </LayoutMainBox>
       <NavBar />
