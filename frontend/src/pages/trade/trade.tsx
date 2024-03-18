828e9e3fc36c565527f982262e227a4fd64ecda4
오정민 <dhwjdals1324@gmail.com>
2024-03-18 11:30:04 +0900

    [feat] menubar 컴포넌트 완성(페이지 이동 제외)

--- frontend/src/pages/trade/trade.tsx
+++ frontend/src/pages/trade/trade.tsx
@@ -1,4 +1,5 @@
 import CheckModal from "../../components/Trade/finishModal";
+import MenuBar from "../../components/Trade/menuBar";
 import MenuBarButton from "../../components/Trade/menuBarButton";
 
 const handelMenuBarClick = () => {};
@@ -6,11 +7,7 @@ const TradePage = () => {
   return (
     <>
       <CheckModal CheckText="거래를 종료하시겠습니까?"></CheckModal>
-      <MenuBarButton
-        onClick={handelMenuBarClick}
-        text="전체"
-        backgroundColor="#A0D8B3"
-      />
+      <MenuBar />
     </>
   );
 };
