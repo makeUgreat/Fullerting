39e07f40c240087be5f7c58e2a34f0836398d909
nayeon <kknyapple@gmail.com>
2024-03-18 16:40:13 +0900

    [feat] : Line component 추가

--- frontend/src/components/common/Line.ts
+++ frontend/src/components/common/Line.ts
@@ -1,9 +1,15 @@
 import styled from "styled-components";
 
 const Line = styled.hr`
-  width: 19.87rem;
-  height: 0.03rem;
-  background: ${({ theme }) => theme.colors.gray2};
+  width: 19.86rem;
+  height: 0.0625rem;
+  background: ${({ theme }) => theme.colors.gray1};
 `;
 
-export { Line };
+const HalfLine = styled.hr`
+  width: 9.93rem;
+  height: 0.0625rem;
+  background: ${({ theme }) => theme.colors.gray1};
+`;
+
+export { HalfLine, Line };
