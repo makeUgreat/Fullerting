c21852e8d15e8300dda79f61ee3795275e69ced8
nayeon <kknyapple@gmail.com>
2024-03-19 16:10:33 +0900

    [feat] : 작물일기 물주기 페이지 layout

---
+++ frontend/src/pages/diary/DiaryWaterPage.tsx
@@ -0,0 +1,47 @@
+import { useState } from "react";
+import { BottomButton } from "../../components/common/Button/LargeButton";
+import StyledInput from "../../components/common/Input/StyledInput";
+import {
+  LayoutInnerBox,
+  LayoutMainBox,
+} from "../../components/common/Layout/Box";
+import { TopBar } from "../../components/common/Navigator/navigator";
+import CropProfile from "../../components/diary/CropProfile";
+import { useAtom } from "jotai";
+import { cropAtom } from "../../stores/diary";
+
+const DiaryWaterPage = () => {
+  const [crop, setCrop] = useAtom(cropAtom);
+  const [selectedDate, setSelectedDate] = useState<string>(
+    new Date().toISOString().slice(0, 10)
+  );
+
+  const handleDateChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSelectedDate(event.target.value);
+  };
+
+  const handleConfirmClick = () => {};
+
+  return (
+    <>
+      <TopBar title="물주기" />
+      <LayoutMainBox>
+        <LayoutInnerBox>
+          <CropProfile crop={crop} />
+          <StyledInput
+            label="날짜 선택하기"
+            type="date"
+            id="date"
+            name="date"
+            placeholder=""
+            value={selectedDate}
+            onChange={handleDateChange}
+          />
+        </LayoutInnerBox>
+      </LayoutMainBox>
+      <BottomButton onClick={handleConfirmClick} text="확인" />
+    </>
+  );
+};
+
+export default DiaryWaterPage;
