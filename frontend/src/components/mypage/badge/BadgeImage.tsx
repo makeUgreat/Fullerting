101714e62835b399149d2b0e211f3ff5bfd99c6e
심우석 <dntjr5741@naver.com>
2024-03-19 17:51:01 +0900

    [feat] : 뱃지 화면 구조화

---
+++ frontend/src/components/mypage/badge/BadgeImage.tsx
@@ -0,0 +1,8 @@
+const BadgeImage = () => {
+  return (
+    <>
+      <h1>전체뱃지</h1>
+    </>
+  );
+};
+export default BadgeImage;
---
+++ frontend/src/components/mypage/badge/BadgeTop.tsx
@@ -0,0 +1,8 @@
+const BadgeTop = () => {
+  return (
+    <>
+      <h1>뱃지 상위</h1>
+    </>
+  );
+};
+export default BadgeTop;
