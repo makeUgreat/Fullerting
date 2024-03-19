0929f9c741dfd0fa1631eb0860dc5a86c5b7058d
nayeon <kknyapple@gmail.com>
2024-03-19 14:12:27 +0900

    [feat] : 작물일지 관련 type 정의

---
+++ frontend/src/types/diary.ts
@@ -0,0 +1,20 @@
+interface CropType {
+  packDiaryId: number;
+  cropType: string;
+  packDiaryTitle: string;
+  packDiaryCulStartAt: string;
+  packDiaryCulEndAt: string | null;
+  packDiaryGrowthStep: string;
+  packDiaryCreatedAt: string;
+  cropTypeImgUrl: string;
+}
+
+interface DiaryType {
+  diaryId: number;
+  packDiaryId: number;
+  diaryBehavior: "다이어리" | "물주기";
+  diaryTitle: string | null;
+  diaryContent: string | null;
+  diarySelectedAt: string;
+  diaryCreatedAt: string;
+}
