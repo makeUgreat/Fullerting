d8e25ee59b8a6bd02af306fa4eedf60f39b94dc5
nayeon <kknyapple@gmail.com>
2024-03-21 16:21:13 +0900

    [feat] : 작물일지 생성 버튼 추가

--- frontend/src/pages/diary/CropPage.tsx
+++ frontend/src/pages/diary/CropPage.tsx
@@ -6,9 +6,27 @@ import { NavBar, TopBar } from "../../components/common/Navigator/navigator";
 import useInput from "../../hooks/useInput";
 import Search from "../../components/common/Input/Search";
 import CropList from "../../components/diary/CropList";
+import styled from "styled-components";
+import { useNavigate } from "react-router-dom";
+
+const PlusButton = styled.button`
+  position: fixed;
+  border-radius: 50%;
+  margin-bottom: 3.625rem;
+  right: 1.25rem;
+  bottom: 1.25rem;
+  width: 3.125rem;
+  height: 3.125rem;
+  background-color: ${({ theme }) => theme.colors.primary};
+`;
 
 const CropPage = () => {
   const [search, setSearch] = useInput("");
+  const navigate = useNavigate();
+
+  const handlePlusButtonClick = () => {
+    navigate("/diary/create");
+  };
 
   return (
     <>
@@ -25,6 +43,23 @@ const CropPage = () => {
           <CropList />
         </LayoutInnerBox>
       </LayoutMainBox>
+      <PlusButton onClick={handlePlusButtonClick}>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="26"
+          height="26"
+          viewBox="0 0 26 26"
+          fill="none"
+        >
+          <path
+            d="M5.70834 13H20.2917M13 5.70837V20.2917"
+            stroke="white"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          />
+        </svg>
+      </PlusButton>
       <NavBar />
     </>
   );
