6d68702e687f844fea1c090999ff3015f2ebe85b
nayeon <kknyapple@gmail.com>
2024-03-21 17:09:02 +0900

    [feat] : 회원가입 api 연결
    POST /users/register

--- frontend/src/apis/UserApi.ts
+++ frontend/src/apis/UserApi.ts
@@ -5,9 +5,26 @@ interface LoginType {
   password: string;
 }
 
+interface JoinType {
+  email: string;
+  password: string;
+  nickname: string;
+  authProvider: "MYAPP";
+}
+
 export const userLogin = async (loginData: LoginType) => {
   try {
     const response = await api.post("/auth/login", loginData);
+    return response.data;
+  } catch (error) {
+    console.error("Error userLogin: ", error);
+    throw error;
+  }
+};
+
+export const userJoin = async (joinData: JoinType) => {
+  try {
+    const response = await api.post("/users/register", joinData);
     return response.data;
   } catch (error) {
     console.error("Error userLogin: ", error);
