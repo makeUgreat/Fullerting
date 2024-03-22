1e11ecc1bf2d7343682521388c33984de7aa0f90
정준원 <junwon1131@naver.com>
2024-03-22 17:42:35 +0900

    websocket 테스트중

--- backend/src/main/java/com/ssafy/fullerting/global/config/WebSocketConfig.java
+++ backend/src/main/java/com/ssafy/fullerting/global/config/WebSocketConfig.java
@@ -16,15 +16,15 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry config) {
-        config.enableSimpleBroker("/sub","/user"); // "/sub"과 "/user"로 시작하는 채널에 대한 구독을 활성화
+        config.enableSimpleBroker("/sub", "/user"); // "/sub"과 "/user"로 시작하는 채널에 대한 구독을 활성화
         config.setApplicationDestinationPrefixes("/pub"); // "/pub"으로 시작하는 메시지는 @MessageMapping 핸들러로 라우팅
     }
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws") // WebSocket 엔드포인트 설정 // ex ) ws://localhost:8080/ws
-                .setAllowedOriginPatterns("*")
-                .withSockJS(); // SockJS 지원
+        registry.addEndpoint("/ws") // WebSocket 엔드포인트 설정 // ex )
+                .setAllowedOriginPatterns("*");
+//                .withSockJS(); // SockJS 지원
     }
 
 
