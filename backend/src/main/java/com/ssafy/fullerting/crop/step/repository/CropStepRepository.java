17e94819900b4ea5c0c157b32576fc5b8d19703e
hyerin <9993hye@naver.com>
2024-03-25 00:49:56 +0900

    [feat] : 작물 생육단계 갱신 API 구현

--- backend/src/main/java/com/ssafy/fullerting/crop/step/repository/CropStepRepository.java
+++ backend/src/main/java/com/ssafy/fullerting/crop/step/repository/CropStepRepository.java
@@ -2,6 +2,7 @@ package com.ssafy.fullerting.crop.step.repository;
 
 import com.ssafy.fullerting.crop.step.model.entity.Step;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -9,4 +10,6 @@ import java.util.Optional;
 @Repository
 public interface CropStepRepository extends JpaRepository<Step, Long> {
     Optional<Step> findByCropIdAndStep(Long cropId, int step);
+    @Query("SELECT MAX(s.step) FROM Step s WHERE s.crop.id = :cropId")
+    Integer findMaxStepByCropId(Long cropId);
 }
