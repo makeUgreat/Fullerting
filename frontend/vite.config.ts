e25eb8bb96b10c2d4cce90a97b3c849f07c11a50
정준원 <junwon1131@naver.com>
2024-03-25 11:11:43 +0900

    websocket test

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -8,10 +8,10 @@ export default defineConfig({
   server: {
     // Proxy 설정
     proxy: {
-      // 경로가 "/api" 로 시작하는 요청을 대상으로 proxy 설정
+      // 경로가 "/websocket" 로 시작하는 요청을 대상으로 proxy 설정
       '/websocket': {
         // 요청 전달 대상 서버 주소 설정
-        target: 'https://j10c102.p.ssafy.io/websocket/ws',
+        target: 'https://j10c102.p.ssafy.io',
         // 요청 헤더 host 필드 값을 대상 서버의 호스트 이름으로  변경
         changeOrigin: true,
         // 요청 경로에서 '/api' 제거
