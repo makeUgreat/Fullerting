6ab219c832fbd4868f8f77b9f2d687aea4d4014b
심우석 <dntjr5741@naver.com>
2024-03-26 02:09:59 +0900

    [feat] : 메인페이지 작물거래 API연결

--- frontend/src/components/main/MainExchange.tsx
+++ frontend/src/components/main/MainExchange.tsx
@@ -1,5 +1,15 @@
 import styled from "styled-components";
 import pullleft from "/src/assets/svg/pullleft.svg";
+import ScrollExchange from "./Exchange";
+import { useQuery } from "@tanstack/react-query";
+import { useState } from "react";
+import { getExchange } from "../../apis/Main";
+import arrow_forward_ios from "../../assets/svg/arrow_forward_ios.svg";
+
+interface ScrollExchangeProps {
+  currentIndex: number;
+  data: any[];
+}
 
 const MainBox = styled.div`
   justify-content: center;
@@ -57,8 +67,46 @@ const ExchangeContainer = styled.div`
   display: flex;
   gap: 1.0625rem;
 `;
+const NextBox = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 0.5rem;
+  background-color: #a0d8b3;
+  border-radius: 50%;
+
+  margin: auto;
+`;
+const NextButton = styled.button`
+  padding: 0;
+  background-color: a0d8b3;
+  background-image: url("${arrow_forward_ios}");
+  background-repeat: no-repeat;
+  background-position: center;
+  width: 1rem;
+  height: 1rem;
+  border: none;
+  cursor: pointer;
+`;
 
 const MainExchange = () => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const { isLoading, error, data } = useQuery({
+    queryKey: ["Exchange"],
+    queryFn: getExchange,
+  });
+
+  const handleNextClick = () => {
+    if (data) {
+      setCurrentIndex((prevIndex) => (prevIndex + 1) % data.length);
+      console.log();
+    }
+  };
+  console.log("여기", data);
+  // 로딩, 에러, 데이터 없음에 대한 처리
+  if (isLoading) return <div>Loading...</div>;
+  if (error) return <div>Error: {error.message}</div>;
+  if (!data || !data || data.length === 0) return <div>거래 데이터 없음</div>;
   return (
     <MainBox>
       <Text>나의 반려작물의 모든 것, 풀러팅에서 함께</Text>
@@ -72,7 +120,12 @@ const MainExchange = () => {
         </LogoContent>
       </LogoAndTextContainer>
       <ExchangeContainer>
-        <ExchangeBox>작물거래</ExchangeBox>
+        <ExchangeBox>
+          <ScrollExchange currentIndex={currentIndex} data={data} />
+        </ExchangeBox>
+        <NextBox>
+          <NextButton onClick={handleNextClick} />
+        </NextBox>
       </ExchangeContainer>
     </MainBox>
   );
