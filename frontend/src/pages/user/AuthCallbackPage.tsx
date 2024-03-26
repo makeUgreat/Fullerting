1701521a4bfcefe9e762c89176e22d5ab6e07680
심우석 <dntjr5741@naver.com>
2024-03-26 21:20:22 +0900

    [fix] 소셜로그인 세션스토리지에 저장

--- frontend/src/pages/user/AuthCallbackPage.tsx
+++ frontend/src/pages/user/AuthCallbackPage.tsx
@@ -5,7 +5,6 @@ const AuthCallbackPage = () => {
   const navigate = useNavigate();
 
   useEffect(() => {
-    // 쿠키에서 accessToken과 refreshToken을 추출하는 함수
     console.log("페이지 들어옴");
     const getCookieValue = (name) => {
       const value = `; ${document.cookie}`;
@@ -14,27 +13,29 @@ const AuthCallbackPage = () => {
       return null;
     };
 
+    const deleteCookie = (name) => {
+      document.cookie = `${name}=; expires=Thu, 01 Jan 1970 00:00:00 GMT; path=/`;
+    };
+
     try {
-      // 쿠키에서 토큰을 가져옵니다.
-      console.log("쿠키에 접근");
       const accessToken = getCookieValue("accessToken");
       const refreshToken = getCookieValue("refreshToken");
+      console.log("쿠키에 접근");
 
-      // 가져온 토큰을 세션 스토리지에 저장합니다.
       if (accessToken && refreshToken) {
         sessionStorage.setItem("accessToken", accessToken);
         sessionStorage.setItem("refreshToken", refreshToken);
         console.log("세션에 저장");
 
-        // 성공적으로 토큰을 저장한 후, 메인 페이지로 리다이렉트합니다.
+        deleteCookie("accessToken");
+        deleteCookie("refreshToken");
+
         navigate("/");
       } else {
-        // 토큰이 존재하지 않는 경우 로그인 페이지로 리다이렉트합니다.
         navigate("/login");
       }
     } catch (e) {
       console.error("토큰 파싱 중 에러가 발생했습니다:", e);
-      // 에러가 발생한 경우 로그인 페이지로 리다이렉트합니다.
       navigate("/login");
     }
   }, [navigate]);
