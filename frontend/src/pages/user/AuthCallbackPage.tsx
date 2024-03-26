e36a58b27c2a125a0f2c53c4b47f2713dd380c77
심우석 <dntjr5741@naver.com>
2024-03-26 19:57:55 +0900

    [fix] 소셜로그인 로직 수정

--- frontend/src/pages/user/AuthCallbackPage.tsx
+++ frontend/src/pages/user/AuthCallbackPage.tsx
@@ -1,43 +1,44 @@
-// AuthCallbackPage.tsx
-import   { useEffect } from "react";
+import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 
 const AuthCallbackPage = () => {
-  console.log("authcallbackpage들어옴");
   const navigate = useNavigate();
 
   useEffect(() => {
-    const preTag = document.querySelector("pre");
-    console.log("1");
-    if (preTag && preTag.textContent) {
-      try {
-        // JSON 파싱을 시도합니다.
-        console.log("2");
-        const tokenData = JSON.parse(preTag.textContent);
-        console.log("3");
-        // 파싱된 데이터에서 액세스 토큰과 리프레시 토큰을 추출합니다.
-        if (tokenData && tokenData.data_body) {
-          sessionStorage.setItem(
-            "accessToken",
-            tokenData.data_body.accessToken
-          );
-          sessionStorage.setItem(
-            "refreshToken",
-            tokenData.data_body.refreshToken
-          );
+    // 쿠키에서 accessToken과 refreshToken을 추출하는 함수
+    console.log("페이지 들어옴");
+    const getCookieValue = (name) => {
+      const value = `; ${document.cookie}`;
+      const parts = value.split(`; ${name}=`);
+      if (parts.length === 2) return parts.pop().split(";").shift();
+      return null;
+    };
 
-          // 성공적으로 토큰을 저장한 후, 메인 페이지로 리다이렉트합니다.
-          navigate("/");
-        }
-      } catch (e) {
-        console.error("토큰 파싱 중 에러가 발생했습니다:", e);
-        // 파싱 중 에러가 발생한 경우 로그인 페이지로 리다이렉트합니다.
+    try {
+      // 쿠키에서 토큰을 가져옵니다.
+      console.log("쿠키에 접근");
+      const accessToken = getCookieValue("accessToken");
+      const refreshToken = getCookieValue("refreshToken");
+
+      // 가져온 토큰을 세션 스토리지에 저장합니다.
+      if (accessToken && refreshToken) {
+        sessionStorage.setItem("accessToken", accessToken);
+        sessionStorage.setItem("refreshToken", refreshToken);
+        console.log("세션에 저장");
+
+        // 성공적으로 토큰을 저장한 후, 메인 페이지로 리다이렉트합니다.
+        navigate("/");
+      } else {
+        // 토큰이 존재하지 않는 경우 로그인 페이지로 리다이렉트합니다.
         navigate("/login");
       }
+    } catch (e) {
+      console.error("토큰 파싱 중 에러가 발생했습니다:", e);
+      // 에러가 발생한 경우 로그인 페이지로 리다이렉트합니다.
+      navigate("/login");
     }
   }, [navigate]);
 
-  // 사용자에게 보여줄 로딩 관련 UI를 렌더링합니다.
   return <div>Loading...</div>;
 };
 
