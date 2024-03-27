cf0cf67ab2f560bae83870fcb981d86e8a4cebeb
문혜린 <9993hye@naver.com>
2024-03-27 13:43:52 +0900

    [fix] : 작물일기 예외처리 및 작물일기 수정 API 수정

--- backend/src/main/java/com/ssafy/fullerting/record/diary/model/dto/request/UpdateDiaryRequest.java
+++ backend/src/main/java/com/ssafy/fullerting/record/diary/model/dto/request/UpdateDiaryRequest.java
@@ -4,14 +4,17 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.time.LocalDate;
+import java.util.List;
 
 @AllArgsConstructor
-@NoArgsConstructor
 @Builder
 @Getter
 public class UpdateDiaryRequest {
+    private List<Long> images;
+    private List<MultipartFile> newImages;
     private String diaryTitle;
     private String diaryContent;
     private LocalDate diarySelectedAt;
