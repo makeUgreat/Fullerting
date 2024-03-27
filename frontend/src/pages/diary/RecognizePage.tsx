8ca406aa144bfa5c53f7ed280625e20ffabd3486
nayeon <kknyapple@gmail.com>
2024-03-27 20:19:53 +0900

    [feat] : 작물인식 로딩중 화면 구현

--- frontend/src/pages/diary/RecognizePage.tsx
+++ frontend/src/pages/diary/RecognizePage.tsx
@@ -11,15 +11,19 @@ import { useAtom } from "jotai";
 import { fileAtom } from "../../stores/diary";
 import AIFileUploadInput from "../../components/common/Input/AIFileUploadInput";
 import { changeStep } from "../../apis/DiaryApi";
+import { useState } from "react";
 
 const RecognizePage = () => {
   const navigate = useNavigate();
   const { packDiaryId } = useParams();
   const [selectedFiles, setSelectedFiles] = useAtom(fileAtom);
+  const [isLoading, setIsLoading] = useState(false);
 
   const { mutate: aiMutate } = useMutation({
     mutationFn: calculateStep,
     onSuccess: (res) => {
+      setIsLoading(false);
+
       const confirmed = window.confirm(
         `${res.crop_type} ${res.grade}단계 입니다.\n작물 단계를 바꿀까요?`
       );
@@ -31,26 +35,28 @@ const RecognizePage = () => {
           packDiaryId: packDiaryId,
           cropStepGrowth: res.grade,
         });
+        navigate(`/crop/${packDiaryId}`);
+        setSelectedFiles([]);
       }
     },
     onError: (error) => {
+      setIsLoading(false);
+      setSelectedFiles([]);
       console.log(error);
     },
   });
 
   const { mutate } = useMutation({
     mutationFn: changeStep,
-    onSuccess: () => {
-      navigate(`/crop/${packDiaryId}`);
-    },
+    onSuccess: () => {},
     onError: () => {
       alert("작물 단계 변경에 실패하였습니다.\n다시 시도해주세요.");
     },
   });
 
   const handleConfirmClick = () => {
+    setIsLoading(true);
     aiMutate(selectedFiles[0]);
-    setSelectedFiles([]);
   };
 
   return (
@@ -58,7 +64,7 @@ const RecognizePage = () => {
       <TopBar title="작물인식" />
       <LayoutMainBox>
         <LayoutInnerBox>
-          <AIFileUploadInput />
+          {isLoading ? <div>로딩중...</div> : <AIFileUploadInput />}
         </LayoutInnerBox>
       </LayoutMainBox>
       <BottomButton onClick={handleConfirmClick} text="확인" />
