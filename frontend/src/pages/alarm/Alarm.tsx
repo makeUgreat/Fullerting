b0c733a6204b9c3a734b59301d937956d06e69fe
심우석 <dntjr5741@naver.com>
2024-03-27 20:15:19 +0900

    [feat] : 알림 페이지 퍼블리싱

--- frontend/src/pages/alarm/Alarm.tsx
+++ frontend/src/pages/alarm/Alarm.tsx
@@ -1,7 +1,28 @@
+import styled from "styled-components";
+import { NavBar, TopBar } from "../../components/common/Navigator/navigator";
+import { LayoutMainBox } from "../../components/common/Layout/Box";
+import AllAlarm from "./AllAlarm";
+
+const LayoutInnerBox = styled.div`
+  display: flex;
+  width: 19.875rem;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding: 1.12rem 0;
+  gap: 0.5rem;
+`;
+
 const Alarm = () => {
   return (
     <>
-      <h1>알람</h1>
+      <TopBar title="알림" />
+      <LayoutMainBox>
+        <LayoutInnerBox>
+          <AllAlarm />
+        </LayoutInnerBox>
+      </LayoutMainBox>
+      <NavBar />
     </>
   );
 };
