e7dc2b463dd705a07898b4e5ead9ece6d42fa145
심우석 <dntjr5741@naver.com>
2024-03-27 22:06:20 +0900

    [feat] : 전체 커뮤니티 필터링, 디테일

--- frontend/src/pages/community/CommunityAll.tsx
+++ frontend/src/pages/community/CommunityAll.tsx
@@ -1,48 +1,54 @@
-import React from "react";
 import styled from "styled-components";
-import likeIcon from "../../assets/svg/like.svg"; // 실제 경로로 대체해야 합니다.
-import commentIcon from "../../assets/svg/classes.svg"; // 실제 경로로 대체해야 합니다.
+import likeIcon from "../../assets/svg/like.svg";
+import commentIcon from "../../assets/svg/classes.svg";
+import { selectedTypeAtom } from "../../stores/community";
+import pullright from "../../assets/svg/pullright.svg";
+import { useAtom } from "jotai";
 
 const posts = [
   {
     id: 1,
-    title: "이게 무슨 식물이야?",
+    title: "자유게시판이게 무슨 식물이야?",
     content: "마리가 풀 삼키려고 하는데 무슨 식물인지 모르겠어...",
-    imageUrl: "/path/to/image",
+    imageUrl: pullright,
     name: "작심삼일",
     time: 13,
     likes: 17,
     comments: 3,
+    type: "자유게시판",
   },
   {
     id: 1,
-    title: "이게 무슨 식물이야?",
+    title: "꿀팁공유이게 무슨 식물이야?",
     content: "마리가 풀 삼키려고 하는데 무슨 식물인지 모르겠어...",
-    imageUrl: "/path/to/image",
+    imageUrl: pullright,
     name: "작심삼일",
     time: 13,
     likes: 17,
     comments: 3,
+    type: "꿀팁공유",
   },
   {
     id: 1,
-    title: "이게 무슨 식물이야?",
+    title: "꿀팁공유이게 무슨 식물이야?",
     content: "마리가 풀 삼키려고 하는데 무슨 식물인지 모르겠어...",
-    imageUrl: "/path/to/image",
+    imageUrl: pullright,
     name: "작심삼일",
     time: 13,
     likes: 17,
     comments: 3,
+    type: "꿀팁공유",
   },
   {
     id: 1,
-    title: "이게 무슨 식물이야?",
+    title: "꿀팁공유이게 무슨 식물이야?",
     content: "마리가 풀 삼키려고 하는데 무슨 식물인지 모르겠어...",
-    imageUrl: "/path/to/image",
+    imageUrl: pullright,
     name: "작심삼일",
     time: 13,
     likes: 17,
     comments: 3,
+    type: "꿀팁공유",
   },
 ];
 
@@ -51,96 +57,126 @@ const CommunityItem = styled.div`
   border-radius: 16px;
   box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
   margin: 0.5rem;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
   padding: 1rem;
+`;
+
+const PostHeader = styled.div`
+  width: 100%;
   display: flex;
   flex-direction: column;
   align-items: flex-start;
 `;
 
-const PostHeader = styled.div`
-  display: flex;
-  justify-content: space-between;
-  width: 100%;
+const PostTitle = styled.h2`
+  color: #000;
+  font-family: "GamtanRoad Dotum TTF";
+  font-size: 0.8rem;
+  font-weight: bold;
   margin-bottom: 0.5rem;
 `;
 
-const PostDetails = styled.div`
-  flex-grow: 1;
-`;
-
-const PostTitle = styled.h2`
-  font-size: 1rem;
-  font-weight: 700;
-  margin: 0;
-`;
-
 const PostContent = styled.p`
-  font-size: 0.875rem;
-  color: #666;
-  margin: 0.5rem 0;
+  color: #000;
+  font-family: "GamtanRoad Dotum TTF";
+  font-size: 0.7rem;
+  font-weight: 400;
+  line-height: 1.2rem;
 `;
 
-const PostNameAndTime = styled.div`
+const PostMeta = styled.div`
+  width: 100%;
   display: flex;
+  justify-content: space-between;
   align-items: center;
-  font-size: 0.75rem;
-  color: #999;
+  margin-bottom: 0.5rem;
+`;
+
+const UserMeta = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
 `;
 
 const UserName = styled.span`
-  margin-right: 0.5rem;
+  color: var(--gray1, #8c8c8c);
+  font-family: "GamtanRoad Dotum TTF";
+  font-size: 0.7rem;
 `;
 
-const PostTime = styled.span``;
-
-const PostImage = styled.img`
-  height: 80px;
-  width: 80px;
-  border-radius: 10px;
-  object-fit: cover;
-  align-self: center;
+const PostTime = styled.span`
+  color: var(--gray1, #8c8c8c);
+  font-family: "Noto Sans KR";
+  font-size: 0.6rem;
+  font-weight: 700;
+  line-height: 1rem;
 `;
 
-const LikeAndComment = styled.div`
+const InteractionIcons = styled.div`
   display: flex;
   align-items: center;
+`;
+
+const Icon = styled.img`
+  height: 1rem;
+  margin: 0 0.25rem;
+`;
+
+const LikeCommentCount = styled.span`
   font-size: 0.75rem;
   color: #999;
-  margin-top: 0.5rem;
 `;
 
-const LikeIcon = styled.img`
-  height: 1rem;
-  width: auto;
-  margin-right: 0.25rem;
+const PostImage = styled.img`
+  width: 5rem;
+  object-fit: cover;
+  border-radius: 10px;
+  margin-bottom: 1rem;
+  margin-left: 1rem;
 `;
 
-const CommentIcon = styled(LikeIcon)``;
+const ContentImage = styled.div`
+  display: flex;
+`;
 
+const NameTime = styled.div``;
+const ContentTitle = styled.div``;
 const CommunityAll = () => {
+  const [selectedType] = useAtom(selectedTypeAtom);
+
   return (
     <div>
-      {posts.map((post) => (
-        <CommunityItem key={post.id}>
-          <PostHeader>
-            <PostDetails>
-              <PostTitle>{post.title}</PostTitle>
-              <PostContent>{post.content}</PostContent>
-              <PostNameAndTime>
-                <UserName>{post.name}</UserName>
-                <PostTime>{post.time}분 전</PostTime>
-              </PostNameAndTime>
-            </PostDetails>
-            <PostImage src={post.imageUrl} alt="Post" />
-          </PostHeader>
-          <LikeAndComment>
-            <LikeIcon src={likeIcon} alt="Likes" />
-            {post.likes}
-            <CommentIcon src={commentIcon} alt="Comments" />
-            {post.comments}
-          </LikeAndComment>
-        </CommunityItem>
-      ))}
+      {posts
+        .filter((post) => post.type === selectedType)
+        .map((post) => (
+          <CommunityItem key={post.id}>
+            <PostHeader>
+              <ContentImage>
+                <ContentTitle>
+                  <PostTitle>{post.title}</PostTitle>
+                  <PostContent>{post.content}</PostContent>
+                </ContentTitle>
+                <PostImage src={post.imageUrl} alt="Post image" />
+              </ContentImage>
+              <PostMeta>
+                <UserMeta>
+                  <NameTime>
+                    <UserName>{post.name} - </UserName>
+                    <PostTime>{post.time}분 전</PostTime>
+                  </NameTime>
+                </UserMeta>
+                <InteractionIcons>
+                  <Icon src={likeIcon} alt="Likes" />
+                  <LikeCommentCount>{post.likes}</LikeCommentCount>
+                  <Icon src={commentIcon} alt="Comments" />
+                  <LikeCommentCount>{post.comments}</LikeCommentCount>
+                </InteractionIcons>
+              </PostMeta>
+            </PostHeader>
+          </CommunityItem>
+        ))}
     </div>
   );
 };
