8150dee89f8bf912f6df4a0505b49b203a7ffea6
kkongch <dhwjdals1324@gmail.com>
2024-03-27 23:38:30 +0900

    [fix] : navigate

--- frontend/src/components/common/Navigator/navigator.tsx
+++ frontend/src/components/common/Navigator/navigator.tsx
@@ -237,7 +237,7 @@ TradeTopBarType) => {
         <TitleBox>{showTitle && title}</TitleBox>
         {showEdit && (
           <EditBox>
-            <EditButton onClick={onClickEdit}>
+            <EditButton onClick={onEdit}>
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 width="24"
