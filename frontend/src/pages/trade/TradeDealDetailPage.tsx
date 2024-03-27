fbfbcf2c0543e8290b1991340cc25d1a3b83ca19
오정민 <dhwjdals1324@gmail.com>
2024-03-27 13:55:03 +0900

    [fix] : tradedetaildeal 수정

---
+++ frontend/src/pages/trade/TradeDealDetailPage.tsx
@@ -0,0 +1,7 @@
+import TradeDetailDeal from "../../components/Trade/TradeDetailDeal";
+// 왜안바껴
+const TradeDealDetailPage = () => {
+  return <TradeDetailDeal />;
+};
+
+export default TradeDealDetailPage;
