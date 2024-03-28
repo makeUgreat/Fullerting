e0e5fdb731a64de75005a1c84600bb1e857f6a8c
김진명 <willom2c@naver.com>
2024-03-28 11:54:03 +0900

    [feat] : 1. 이벤트 알람객체 빌더패턴 리팩토링

--- backend/src/main/java/com/ssafy/fullerting/alarm/model/entity/EventAlarm.java
+++ backend/src/main/java/com/ssafy/fullerting/alarm/model/entity/EventAlarm.java
@@ -19,12 +19,12 @@ public class EventAlarm {
     private Long alarmId;
 
     @ManyToOne
-    @JoinColumn(name = "user_id")
-    private CustomUser customUser;
+    @JoinColumn(name = "receive_user_id")
+    private CustomUser receiveUserId;
 
     @ManyToOne
-    @JoinColumn(name = "user_id")
-    private CustomUser fromUserId;
+    @JoinColumn(name = "send_user_id")
+    private CustomUser sendUserId;
 
     @Column(name = "event_alarm_type", nullable = false, length = 20)
     @Enumerated(EnumType.STRING)
@@ -33,12 +33,76 @@ public class EventAlarm {
     @Column(name = "event_alarm_content", nullable = false, length = 200)
     private String content;
 
-    @CreationTimestamp()
+    @CreationTimestamp
     private LocalDateTime createdAt;
 
     @Column(name = "event_alarm_is_checked")
-    private boolean isChecked;
+    private boolean isChecked = false;
 
-    @Column(name = "event_alram_redirect", nullable = false, length = 255)
+    @Column(name = "event_alarm_redirect", nullable = false, length = 255)
     private String redirect;
+
+
+    // JPA 프록시 객체 생성을 위해 기본 생성자는 필수
+    protected EventAlarm() {}
+
+    // 빌더 생성자 -> 빌더 객체로부터 값을 받아 EventAlarm 초기화 
+    public EventAlarm(Builder builder) {
+        this.receiveUserId = builder.receiveUserId;
+        this.sendUserId = builder.sendUserId;
+        this.type = builder.type;
+        this.content = builder.content;
+        this.isChecked = builder.isChecked;
+        this.redirect = builder.redirect;
+    }
+    
+    
+    // EventAlarm 클래스이 정적 팩토리 메서드
+    public static Builder builder() {
+        return new Builder();
+    }
+
+    public static class Builder {
+        private CustomUser receiveUserId;
+        private CustomUser sendUserId;
+        private EventAlarmType type;
+        private String content;
+        private boolean isChecked = false;
+        private String redirect;
+
+
+        public Builder receiveUserId(CustomUser receiveUserId) {
+            this.receiveUserId = receiveUserId;
+            return this;
+        }
+
+        public Builder sendUserId(CustomUser sendUserId) {
+            this.sendUserId = sendUserId;
+            return this;
+        }
+
+        public Builder type(EventAlarmType type) {
+            this.type = type;
+            return this;
+        }
+
+        public Builder content(String content) {
+            this.content = content;
+            return this;
+        }
+
+        public Builder isChecked(boolean isChecked) {
+            this.isChecked = isChecked;
+            return this;
+        }
+
+        public Builder redirect(String redirect) {
+            this.redirect = redirect;
+            return this;
+        }
+
+        public EventAlarm build() {
+            return new EventAlarm(this);
+        }
+    }
 }
--- backend/src/main/java/com/ssafy/fullerting/alarm/service/EventAlarmService.java
+++ backend/src/main/java/com/ssafy/fullerting/alarm/service/EventAlarmService.java
@@ -1,11 +1,17 @@
 package com.ssafy.fullerting.alarm.service;
 
+import com.ssafy.fullerting.alarm.repository.EventAlarmRepository;
+import com.ssafy.fullerting.exArticle.model.dto.response.ExArticleResponse;
+import com.ssafy.fullerting.user.model.dto.response.UserResponse;
+import com.ssafy.fullerting.user.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class EventAlarmService {
+    private final UserService userService;
+    private final EventAlarmRepository eventAlarmRepository;
     // 알림 트리거
     // 1. 내가 쓴 게시물에 댓글이 달렸을 때
     // 2. 채팅이 왔을 때 (채팅이 생성됐을 때)
@@ -16,7 +22,11 @@ public class EventAlarmService {
 
 
     // 현재 사용자의 알림함에 저장하는 메서드
-    public void saveAlarm() {
+    // 실행조건 : 입찰자가 입찰하기를 눌렀을 때
+    public void notifyAuctionBidReceived(Long bidUserId, ExArticleResponse exArticleResponse) {
+        // 내가 가격제안 게시물을 올렸는데
+        // 누군가가 입찰을 했을 때 알림
+        Long receiveUserId = exArticleResponse.getExArticleId();
 
 
     }
