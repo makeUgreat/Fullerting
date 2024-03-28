79ec066d81078a2be3cbb8ab4ae41f7601b4c197
nayeon <kknyapple@gmail.com>
2024-03-28 17:03:39 +0900

    [feat] : 지도 마커 커스텀

--- frontend/src/components/garden/GardenMap.tsx
+++ frontend/src/components/garden/GardenMap.tsx
@@ -1,5 +1,6 @@
-import { Map } from "react-kakao-maps-sdk";
+import { Map, MapMarker } from "react-kakao-maps-sdk";
 import useKakaoLoader from "../../hooks/useKakaoLoader";
+import shovelImg from "../../assets/images/shovel.png";
 
 const GardenMap = () => {
   useKakaoLoader();
@@ -16,7 +17,27 @@ const GardenMap = () => {
         height: "28rem",
       }}
       level={3}
-    />
+    >
+      <MapMarker
+        position={{
+          lat: 35.2059392,
+          lng: 126.81216,
+        }}
+        image={{
+          src: shovelImg,
+          size: {
+            width: 64,
+            height: 69,
+          },
+          options: {
+            offset: {
+              x: 27,
+              y: 69,
+            },
+          },
+        }}
+      />
+    </Map>
   );
 };
 
