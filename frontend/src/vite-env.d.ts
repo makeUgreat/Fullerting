379aa0405935410a18339a8397cac98aa5f68d24
nayeon <kknyapple@gmail.com>
2024-03-28 16:48:59 +0900

    [feat] : 지도 환경 설정

--- frontend/src/vite-env.d.ts
+++ frontend/src/vite-env.d.ts
@@ -4,11 +4,9 @@ interface ImportMetaEnv {
   readonly VITE_CUSTOM_ENV_VARIABLE: string;
   readonly VITE_SERVER_URL: string;
   readonly __WEBSOCKET_URL__: string;
-
+  readonly VITE_KAKAO_MAP_KEY: string;
 }
 
 interface ImportMeta {
   readonly env: ImportMetaEnv;
 }
-
- 
