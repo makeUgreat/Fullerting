bc773b16d00693eda912feba5cc0af8e05dc4eaf
nayeon <kknyapple@gmail.com>
2024-03-29 10:00:03 +0900

    [feat] : 지도 클러스터 추가

--- frontend/src/components/garden/GardenMap.tsx
+++ frontend/src/components/garden/GardenMap.tsx
@@ -1,4 +1,4 @@
-import { Map, MapMarker } from "react-kakao-maps-sdk";
+import { Map, MapMarker, MarkerClusterer } from "react-kakao-maps-sdk";
 import useKakaoLoader from "../../hooks/useKakaoLoader";
 import shovelImg from "../../assets/images/shovel.png";
 import { useState } from "react";
@@ -121,50 +121,54 @@ const GardenMap = () => {
         width: "100%",
         height: "28rem",
       }}
-      level={7}
+      level={14}
     >
-      {gardenDummy.map((farm) => (
-        <MapMarker
-          position={{
-            lat: farm.farmPosLat,
-            lng: farm.farmPosLng,
-          }}
-          image={{
-            src: shovelImg,
-            size: {
-              width: 50,
-              height: 55,
-            },
-            options: {
-              offset: {
-                x: 27,
-                y: 69,
-              },
-            },
-          }}
-          clickable={true} // 마커를 클릭했을 때 지도의 클릭 이벤트가 발생하지 않도록 설정합니다
-          onClick={() => setIsOpen(true)}
-        >
-          {isOpen && (
-            <div style={{ minWidth: "150px" }}>
-              <img
-                alt="close"
-                width="14"
-                height="13"
-                src="https://t1.daumcdn.net/localimg/localimages/07/mapjsapi/2x/bt_close.gif"
-                style={{
-                  position: "absolute",
-                  right: "5px",
-                  top: "5px",
-                  cursor: "pointer",
-                }}
-                onClick={() => setIsOpen(false)}
-              />
-              <div style={{ padding: "0.28rem" }}>{farm.farmName}</div>
-            </div>
-          )}
-        </MapMarker>
-      ))}
+      <MarkerClusterer averageCenter={true} minLevel={10}>
+        {gardenDummy &&
+          gardenDummy.map((farm) => (
+            <MapMarker
+              key={`${farm.farmPosLat}-${farm.farmPosLng}`}
+              position={{
+                lat: farm.farmPosLat,
+                lng: farm.farmPosLng,
+              }}
+              image={{
+                src: shovelImg,
+                size: {
+                  width: 50,
+                  height: 55,
+                },
+                options: {
+                  offset: {
+                    x: 27,
+                    y: 69,
+                  },
+                },
+              }}
+              clickable={true} // 마커를 클릭했을 때 지도의 클릭 이벤트가 발생하지 않도록 설정합니다
+              onClick={() => setIsOpen(true)}
+            >
+              {isOpen && (
+                <div style={{ minWidth: "150px" }}>
+                  <img
+                    alt="close"
+                    width="14"
+                    height="13"
+                    src="https://t1.daumcdn.net/localimg/localimages/07/mapjsapi/2x/bt_close.gif"
+                    style={{
+                      position: "absolute",
+                      right: "5px",
+                      top: "5px",
+                      cursor: "pointer",
+                    }}
+                    onClick={() => setIsOpen(false)}
+                  />
+                  <div style={{ padding: "0.28rem" }}>{farm.farmName}</div>
+                </div>
+              )}
+            </MapMarker>
+          ))}
+      </MarkerClusterer>
     </Map>
   );
 };
