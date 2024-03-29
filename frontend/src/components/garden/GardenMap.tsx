932667c4a160d9d6094896a707839885e2c1fdf0
nayeon <kknyapple@gmail.com>
2024-03-29 11:23:16 +0900

    [fix] : 지도마커 컴포넌트 분리
    인포윈도우 오류 해결

--- frontend/src/components/garden/GardenMap.tsx
+++ frontend/src/components/garden/GardenMap.tsx
@@ -1,9 +1,9 @@
-import { Map, MapMarker, MarkerClusterer, useMap } from "react-kakao-maps-sdk";
+import { Map, MarkerClusterer, useMap } from "react-kakao-maps-sdk";
 import useKakaoLoader from "../../hooks/useKakaoLoader";
-import shovelImg from "../../assets/images/shovel.png";
 import { useEffect, useMemo, useState } from "react";
 import { useQuery } from "@tanstack/react-query";
 import { getGardenList } from "../../apis/GardenApi";
+import GardenMarker from "./GardenMarker";
 
 const ReSettingMapBounds = ({ farmList }: { farmList: FarmType[] }) => {
   const map = useMap();
@@ -23,8 +23,6 @@ const ReSettingMapBounds = ({ farmList }: { farmList: FarmType[] }) => {
 };
 
 const GardenMap = () => {
-  const [isOpen, setIsOpen] = useState(false);
-
   useKakaoLoader();
 
   const { data: farmList } = useQuery({
@@ -48,47 +46,10 @@ const GardenMap = () => {
       <MarkerClusterer averageCenter={true} minLevel={10}>
         {farmList &&
           farmList.map((farm: FarmType) => (
-            <MapMarker
+            <GardenMarker
               key={`${farm.farmPosLat}-${farm.farmPosLng}`}
-              position={{
-                lat: farm.farmPosLat,
-                lng: farm.farmPosLng,
-              }}
-              image={{
-                src: shovelImg,
-                size: {
-                  width: 50,
-                  height: 55,
-                },
-                options: {
-                  offset: {
-                    x: 27,
-                    y: 69,
-                  },
-                },
-              }}
-              clickable={true} // 마커를 클릭했을 때 지도의 클릭 이벤트가 발생하지 않도록 설정합니다
-              onClick={() => setIsOpen(true)}
-            >
-              {isOpen && (
-                <div style={{ minWidth: "150px" }}>
-                  <img
-                    alt="close"
-                    width="14"
-                    height="13"
-                    src="https://t1.daumcdn.net/localimg/localimages/07/mapjsapi/2x/bt_close.gif"
-                    style={{
-                      position: "absolute",
-                      right: "5px",
-                      top: "5px",
-                      cursor: "pointer",
-                    }}
-                    onClick={() => setIsOpen(false)}
-                  />
-                  <div style={{ padding: "0.28rem" }}>{farm.farmName}</div>
-                </div>
-              )}
-            </MapMarker>
+              farm={farm}
+            />
           ))}
       </MarkerClusterer>
       {farmList && <ReSettingMapBounds farmList={farmList} />}
---
+++ frontend/src/components/garden/GardenMarker.tsx
@@ -0,0 +1,55 @@
+import { useState } from "react";
+import { MapMarker } from "react-kakao-maps-sdk";
+import shovelImg from "../../assets/images/shovel.png";
+
+const GardenMarker = ({ farm }: { farm: FarmType }) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <>
+      <MapMarker
+        key={`${farm.farmPosLat}-${farm.farmPosLng}`}
+        position={{
+          lat: farm.farmPosLat,
+          lng: farm.farmPosLng,
+        }}
+        image={{
+          src: shovelImg,
+          size: {
+            width: 45,
+            height: 50,
+          },
+          options: {
+            offset: {
+              x: 27,
+              y: 69,
+            },
+          },
+        }}
+        clickable={true} // 마커를 클릭했을 때 지도의 클릭 이벤트가 발생하지 않도록 설정합니다
+        onClick={() => setIsOpen(true)}
+      >
+        {isOpen && (
+          <div style={{ minWidth: "150px" }}>
+            <img
+              alt="close"
+              width="14"
+              height="13"
+              src="https://t1.daumcdn.net/localimg/localimages/07/mapjsapi/2x/bt_close.gif"
+              style={{
+                position: "absolute",
+                right: "5px",
+                top: "5px",
+                cursor: "pointer",
+              }}
+              onClick={() => setIsOpen(false)}
+            />
+            <div style={{ padding: "0.28rem" }}>{farm.farmName}</div>
+          </div>
+        )}
+      </MapMarker>
+    </>
+  );
+};
+
+export default GardenMarker;
