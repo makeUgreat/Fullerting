777179fda5b1fc6d6f46998d66999d76808f9ef0
kkongch <dhwjdals1324@gmail.com>
2024-03-30 19:23:09 +0900

    [fix] : sse

--- frontend/src/pages/trade/TradeGeneralDetail.tsx
+++ frontend/src/pages/trade/TradeGeneralDetail.tsx
@@ -1,6 +1,8 @@
 import TradeGeneralDetail from "../../components/Trade/TradeDetailGeneral";
+import { useSSEConnection } from "../../hooks/useSSEConnection";
 
 const TradeGeneralDetailPage = () => {
+  useSSEConnection();
   return <TradeGeneralDetail />;
 };
 
