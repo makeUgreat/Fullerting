e7192e54f241a7a0fd1157760c068aa1a8a466e2
김진명 <willom2c@naver.com>
2024-04-01 20:47:57 +0900

    "[fix] : 1. 알림함 게시물 제목 색상, 볼드 처리 2. 알림함 생성일 순으로 내림차순 정렬

--- frontend/src/pages/alarm/AllAlarm.tsx
+++ frontend/src/pages/alarm/AllAlarm.tsx
@@ -21,14 +21,14 @@ const Notification = styled.div<NotificationProps>`
   display: flex;
   align-items: center;
   font-family: "GamtanRoad Dotum TTF";
-  font-size: 0.7rem;
+  font-size: 0.73rem;
 
   &::before {
     content: "";
     display: block;
     width: 0.75rem;
     height: 0.75rem;
-    margin-right: 10px;
+    margin-right: 5px;
     background-image: url(${(props) =>
       props.alarmType === "커뮤니티" ? diaryIcon : cartIcon});
     background-size: cover;
@@ -37,12 +37,12 @@ const Notification = styled.div<NotificationProps>`
 `;
 const NotificationItem = styled.div<{ isNew: boolean }>`
   font-family: "GamtanRoad Dotum TTF";
-  font-size: 0.8rem;
+  font-size: 0.9rem;
   font-weight: 400;
-  line-height: 1.7rem;
+  line-height: 2rem;
   width: 23.45rem;
   height: 5.5rem;
-  padding: 0.5rem 1.5rem;
+  padding: 0.6rem 1.8rem;
   background-color: ${(props) => (props.isNew ? "none" : "#E5F9DB")};
   border-bottom: 1px solid #e5e7eb;
   cursor: pointer;
@@ -50,6 +50,9 @@ const NotificationItem = styled.div<{ isNew: boolean }>`
   &:first-child {
     border-top: 1.5px solid #e5e7eb;
   }
+
+  min-height: 5.5rem; /* 고정된 높이에서 최소 높이로 변경 */
+  height: auto; /* 높이를 자동으로 조정하도록 설정 */
 `;
 
 const AllAlarm = () => {
@@ -72,7 +75,8 @@ const AllAlarm = () => {
     console.error("알람 데이터를 가져오는데 실패했습니다:", error);
     return <div>알람 데이터를 가져오는데 실패했습니다: {error.message}</div>;
   }
-  console.log("alarm", mockNotifications);
+  // console.log("alarm", mockNotifications);
+  const sortedNotifications = [...mockNotifications].sort((a, b) => b.alarmId - a.alarmId);
 
   const parseContent = (content: string): React.ReactNode => {
     const regex = /#(.*?)#/g;
@@ -80,23 +84,23 @@ const AllAlarm = () => {
 
     return parts.map((part, index) => {
       if (index % 2 === 1) {
-        return <strong key={index}>{part}</strong>;
+        return <strong key={index} style={{ color: '#A0D8B3', fontWeight: 'bold' }} >{part}</strong>;
       } else {
         return part;
       }
     });
   };
-  const sortedNotifications = mockNotifications.slice().sort((a, b) => {
-    return Number(a.checked) - Number(b.checked);
-  });
+  // const sortedNotifications = mockNotifications.slice().sort((a, b) => {
+  //   return Number(a.checked) - Number(b.checked);
+  // });
 
   const handleClick = async (alarmId: number) => {
     try {
       await readAlarm(alarmId);
-      const notification = mockNotifications.find(
-        (notification) => notification.alarmId === alarmId
+      const notification = sortedNotifications.find(
+        notification => notification.alarmId === alarmId
       );
-      console.log(`Notification ${alarmId} clicked.`);
+      // console.log(`Notification ${alarmId} clicked.`);
       navigate(notification.alarmRedirect);
     } catch (error) {
       console.error(`알람 ${alarmId} 처리 중 오류 발생: `, error);
@@ -114,7 +118,7 @@ const AllAlarm = () => {
           >
             <Notification alarmType={notification.alarmType}>
               {notification.alarmType}
-              {notification.alarmId}
+              {/* {notification.alarmId} */}
             </Notification>
             <div>{parseContent(notification.alarmContent)}</div>
           </NotificationItem>
