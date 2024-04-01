94514c957b3421839a0606b2966cd7cbb3e91c49
김진명 <willom2c@naver.com>
2024-04-01 12:16:07 +0900

    [fix] : S3 사진업로드 예외처리 추가 - 확장자가 다를 경우 - 빈 파일로 업로드 했을 경우

--- backend/src/main/java/com/ssafy/fullerting/global/s3/exception/S3ErrorCode.java
+++ backend/src/main/java/com/ssafy/fullerting/global/s3/exception/S3ErrorCode.java
@@ -1,27 +1,16 @@
-//package com.ssafy.fullerting.global.s3.exception;
-//
-//import lombok.AllArgsConstructor;
-//import lombok.Getter;
-//import org.springframework.http.HttpStatus;
-//
-//
-//@Getter
-//@AllArgsConstructor
-//public class S3ErrorCode {
-//    INVALID_EXTENSION("jpg, jpeg, svg, png 파일만 등록 가능합니다", HttpStatus.BAD_REQUEST)
-//
-//    private final String message;
-//    private final HttpStatus httpStatus;
-//}
-//
-//
-////@Getter
-////@AllArgsConstructor
-////public enum UserErrorCode {
-////    NOT_EXISTS_USER("존재하지 않는 회원입니다.", HttpStatus.BAD_REQUEST),
-////    ALREADY_IN_EMAIL("이미 가입된 회원입니다.", HttpStatus.BAD_REQUEST),
-////    ACCESS_DENIED("권한이 없습니다.", HttpStatus.FORBIDDEN);
-////
-////    private final String message;
-////    private final HttpStatus httpStatus;
-////}
+package com.ssafy.fullerting.global.s3.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+
+@Getter
+@AllArgsConstructor
+public enum  S3ErrorCode {
+    INVALID_EXTENSION("[확장자오류] : jpg, jpeg, svg, png 파일만 등록 가능합니다", HttpStatus.BAD_REQUEST),
+    EMPTY_UPLOAD_FILE("[빈파일오류] : 업로드 파일이 없습니다", HttpStatus.BAD_REQUEST);
+
+    private final String message;
+    private final HttpStatus httpStatus;
+}
