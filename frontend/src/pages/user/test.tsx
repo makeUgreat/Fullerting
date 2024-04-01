bc3cc25f2fa1b6147bb952df7ad2b86780552725
김진명 <willom2c@naver.com>
2024-04-01 10:21:04 +0900

    [fix] : 프론트 url env 전역관리

--- frontend/src/pages/user/test.tsx
+++ frontend/src/pages/user/test.tsx
@@ -86,7 +86,7 @@ function TestPage() {
     }
 
     //  const socket = new WebSocket("ws://localhost:8080/ws");
-    const socket = new WebSocket("wss://j10c102.p.ssafy.io/api/ws");
+    const socket = import.meta.env.VITE_REACT_APP_WSS_URL;
     const client = Stomp.over(socket);
 
     console.log(socket);
